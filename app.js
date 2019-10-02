9ddf466e92c214ad2b2c1f6b5e2c7e76f28e317b
Rhythmvashist <vashistrhythm1@gmail.com>
2019-10-02 00:28:14 -0700

    layed the pipeline of html file

---
+++ app.js
@@ -0,0 +1,8 @@
+const form=document.querySelector('#task-form')
+const tasklist=document.querySelector('.collection')
+const clearBtn=document.querySelector('.clear-tasks')
+const filer=document.getElementById('filter')
+const taskInput=document.getElementById('task')
+
+// load all event listners
+loadAllEventListners()
